c5e51a7a4b4c3d5e8ac524cb67d5a1221fa45645
Brian Engen <engenb@gmail.com>
2016-11-16 00:00:36 -0600

    just adding some garbage so we can test a pull request CI buid in vsts

--- src/AssemblyToWeave/Patient.cs
+++ src/AssemblyToWeave/Patient.cs
@@ -21,6 +21,9 @@ namespace AssemblyToWeave
         [Property]
         public decimal AccountBalance { get; set; }
 
+		[Property]
+		public string FavoriteFood { get; set; }
+
         public Provider Provider { get; set; }
     }
 }
