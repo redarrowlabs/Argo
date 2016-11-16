0feda671d66b88f5c13670f5ac32c785bfb49d55
Brian Engen <engenb@gmail.com>
2016-11-16 00:29:12 -0600

    again

--- src/AssemblyToWeave/Patient.cs
+++ src/AssemblyToWeave/Patient.cs
@@ -20,7 +20,10 @@ namespace AssemblyToWeave
 
         [Property]
         public decimal AccountBalance { get; set; }
-		
+
+		[Property]
+		public string FavoriteFood { get; set; }
+
         public Provider Provider { get; set; }
     }
 }
