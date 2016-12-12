17133ee4a941248ada0dbdfc8b271379bad319e8
Brian Engen <engenb@gmail.com>
2016-12-12 17:25:49 -0600

    removing these -pre tags from local testing (facepalm)

--- src/RedArrow.Jsorm.Client/Properties/AssemblyInfo.cs
+++ src/RedArrow.Jsorm.Client/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("jsorm client tools for session management and model retrieval")]
 
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0-pre")]
-[assembly: AssemblyInformationalVersion("0.0.0.0-pre")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
 [assembly: InternalsVisibleTo("RedArrow.Jsorm.Client.Tests")]
