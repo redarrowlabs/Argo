50ad46899a1cd4ad708a8f817f0cf250add46f15
Brian Engen <engenb@gmail.com>
2017-02-13 13:15:44 -0600

    Revert "rearranged WovenByFody projects to test by .netFx and pcl assemly weaving.  fixed pcl weaving (#24)"
    This reverts commit fd670f5935eed1597bd3d4984c5430fb81d41f6a.

--- src/RedArrow.Argo.Fody/TypeDefLoader.cs
+++ src/RedArrow.Argo.Fody/TypeDefLoader.cs
@@ -38,15 +38,10 @@ namespace RedArrow.Argo
                 .Methods
                 .SingleOrDefault(x => x.Name == "SetGenericCollection");
 
-            _guidTypeDef = argoAssemblyDef.MainModule
-                .ImportReference(typeof(System.Guid))
-                .Resolve();
-            _genericIEnumerableTypeDef = argoAssemblyDef.MainModule
-                .ImportReference(typeof(System.Collections.Generic.IEnumerable<>))
-                .Resolve();
-            _genericICollectionTypeDef = argoAssemblyDef.MainModule
-                .ImportReference(typeof(System.Collections.Generic.ICollection<>))
-                .Resolve();
+            var msCoreAssemblyDef = AssemblyResolver.Resolve("mscorlib");
+            _guidTypeDef = msCoreAssemblyDef.MainModule.GetType("System.Guid");
+            _genericIEnumerableTypeDef = msCoreAssemblyDef.MainModule.GetType("System.Collections.Generic.IEnumerable`1");
+            _genericICollectionTypeDef = msCoreAssemblyDef.MainModule.GetType("System.Collections.Generic.ICollection`1");
 
             _string_equals = ModuleDefinition
                 .TypeSystem
@@ -60,16 +55,12 @@ namespace RedArrow.Argo
                             x.Parameters[1].ParameterType.Name == "String" &&
                             x.Parameters[2].ParameterType.Name == "StringComparison");
 
-            _stringComparison_ordinal = (int)argoAssemblyDef.MainModule
-                .ImportReference(typeof(System.StringComparison))
-                .Resolve()
+            _stringComparison_ordinal = (int)msCoreAssemblyDef.MainModule.GetType("System.StringComparison")
                 .Fields
                 .First(x => x.Name == "Ordinal")
                 .Constant;
 
-            _equalityComparerTypeDef = argoAssemblyDef.MainModule
-                .ImportReference(typeof(System.Collections.Generic.EqualityComparer<>))
-                .Resolve();
+            _equalityComparerTypeDef = msCoreAssemblyDef.MainModule.GetType("System.Collections.Generic.EqualityComparer`1");
 
             _object_equals = ModuleDefinition
                 .TypeSystem
--- src/WovenByFody/Properties/AssemblyInfo.cs
+++ src/WovenByFody/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WovenByFody.PCL")]
+[assembly: AssemblyTitle("AssemblyToWeave")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WovenByFody.PCL")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("AssemblyToWeave")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2aa3dfdf-f71d-4ace-bfd5-0100ec753bc8")]
 
 // Version information for an assembly consists of the following four values:
 //
