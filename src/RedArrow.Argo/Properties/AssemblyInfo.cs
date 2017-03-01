38007b90eccbf832d4a48762c0e17057d743708d
Brian Engen <engenb@gmail.com>
2017-03-01 16:39:30 -0600

    version bump

--- src/RedArrow.Argo/Properties/AssemblyInfo.cs
+++ src/RedArrow.Argo/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("RedArrow.Argo.Tests")]
 
-[assembly: AssemblyVersion("0.0.29.0")]
-[assembly: AssemblyFileVersion("0.0.29.0")]
-[assembly: AssemblyInformationalVersion("0.0.29.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
