77c9e1648e6e9a7aea32a509ad5beb33bb7cabf0
Brian Engen <bengen@redarrowlabs.com>
2017-04-27 13:01:56 -0500

    some simple nuspec and copyright updates

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Red Arrow")]
 [assembly: AssemblyProduct("Argo")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
